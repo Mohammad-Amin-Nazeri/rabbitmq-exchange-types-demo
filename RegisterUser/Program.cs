532fae355ba1d06218803494a4808be8e8f2a5ae
Mohammad Amin Nazeri <nazrymhmdamyn85@gmail.com>
2026-06-16 20:46:35 +0330

    Add project files.

---
+++ RegisterUser/Program.cs
@@ -0,0 +1,28 @@
+﻿//
+
+using System.Text;
+using RabbitMQ.Client;
+
+var queueName = "register-User";
+Console.Write("Enter Your PhoneNumber :");
+var phoneNumber = Console.ReadLine();
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+var connection = await connectionFactory.CreateConnectionAsync();
+var model = await connection.CreateChannelAsync();
+await model.QueueDeclareAsync(queueName, true, false, false, null);
+
+
+if (phoneNumber != null)
+{
+    var body = Encoding.ASCII.GetBytes(phoneNumber.ToString());
+    await model.BasicPublishAsync("", queueName, body);
+}
+
+Console.WriteLine(phoneNumber);
+Console.ReadKey(); 
