064fddd4d3976ae3234feb60760c15bc98c61891
Mohammad Amin Nazeri <nazrymhmdamyn85@gmail.com>
2026-06-20 10:20:14 +0330

    Direct-Exchange

---
+++ Direct-Exchange/SendSms.Direct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var queueName = "register-User";
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+var connection = await connectionFactory.CreateConnectionAsync();
+var model = await connection.CreateChannelAsync();
+await model.QueueDeclareAsync(queueName, true, false, false, null);
+
+var consumer = new AsyncEventingBasicConsumer(model);
+
+consumer.ReceivedAsync += async (sender, args) =>
+{
+    var result = Encoding.ASCII.GetString(args.Body.ToArray());
+    Console.WriteLine("Send Sms For " + result);
+};
+
+await model.BasicConsumeAsync(queueName, true, consumer);
+
+Console.Read();
